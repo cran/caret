e7b56219e9a68db65625c4cbb74df71424facfc6
Max Kuhn <Max.Kuhn@pfizer.com>
2009-10-28 17:13:10 +0000

    version 4.26

--- R/lmVarImp.R
+++ R/lmVarImp.R
@@ -1,6 +1,7 @@
 varImp.lm <- function(object, ...) 
 {
-   varImps <-  abs(summary(object)$coef[-1, "t value"])
+  values <- summary(object)$coef
+   varImps <-  abs(values[-1, grep("value$", colnames(values))])
    out <- data.frame(varImps)
    colnames(out) <- "Overall"
    if(!is.null(names(varImps))) rownames(out) <- names(varImps)
