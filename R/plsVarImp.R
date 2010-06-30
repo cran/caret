04d1be8382f62485782992a20433c4a8774c9f0a
Max Kuhn <Max.Kuhn@pfizer.com>
2010-06-30 00:00:00 +0000

    version 4.43

--- R/plsVarImp.R
+++ R/plsVarImp.R
@@ -1,21 +1,29 @@
-varImp.mvr <- function(object, ...)
+varImp.mvr <- function(object, estimate = NULL, ...)
 {
-   library(pls)
-   modelCoef <- coef(object, intercept = FALSE, comps = 1:object$ncomp)
-   perf <- MSEP(object)$val
-   
-   numResp <- dim(modelCoef)[2]
-   
-   if(numResp <= 2)
-   {
+  library(pls)
+  modelCoef <- coef(object, intercept = FALSE, comps = 1:object$ncomp)
+  perf <- MSEP(object)$val
+
+  nms <- dimnames(perf)
+  if(length(nms$estimate) > 1)
+    {
+      pIndex <- if(is.null(estimate)) 1 else which(nms$estimate == estimate)
+      perf <- perf[pIndex,,,drop = FALSE]
+    }  
+  
+  numResp <- dim(modelCoef)[2]
+  
+  if(numResp <= 2)
+    {
       modelCoef <- modelCoef[,1,,drop = FALSE]
+
       perf <- perf[,1,]
       delta <- -diff(perf)
       delta <- delta/sum(delta)
       out <- data.frame(
-         Overall = apply(abs(modelCoef), 1, weighted.mean, w = delta))
+                        Overall = apply(abs(modelCoef), 1, weighted.mean, w = delta))
       
-   } else {
+    } else {
       
       perf <- -t(apply(perf[1,,], 1, diff))
       perf <- t(apply(perf, 1, function(u) u/sum(u)))
@@ -23,14 +31,14 @@ varImp.mvr <- function(object, ...)
       out <- matrix(NA, ncol = numResp, nrow = dim(modelCoef)[1])
       
       for(i in 1:numResp)
-      {
-         tmp <- abs(modelCoef[,i,, drop = FALSE])
-         out[,i] <- apply(tmp, 1,  weighted.mean, w = perf[i,])
-      }
+        {
+          tmp <- abs(modelCoef[,i,, drop = FALSE])
+          out[,i] <- apply(tmp, 1,  weighted.mean, w = perf[i,])
+        }
       colnames(out) <- dimnames(modelCoef)[[2]]
       rownames(out) <- dimnames(modelCoef)[[1]]
       
-   }
-   as.data.frame(out)
+    }
+  as.data.frame(out)
 }
 
